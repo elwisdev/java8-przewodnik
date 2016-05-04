b29607c8d50fb4aac87bcf892f4386e756283641
WISNIEWSKI.LUKASZ <lukasz.wisniewski@interia.pl>
2016-05-04 22:08:16 +0200

    start

--- r03ex/ex08/Greeter.java
+++ r03ex/ex08/Greeter.java
@@ -3,5 +3,12 @@ package r03ex.ex08;
 /**
  * Created by WISNIEWSKI.LUKASZ on 2016-05-04.
  */
-public class Greeter {
+public class Greeter implements Runnable {
+
+    public Greeter(){}
+
+    @Override
+    public void run() {
+
+    }
 }
