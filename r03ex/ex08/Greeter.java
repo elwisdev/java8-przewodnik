711d54b89bae21b4e2a61ed57d5dc2cc799ce1e2
WISNIEWSKI.LUKASZ <lukasz.wisniewski@interia.pl>
2016-05-04 22:24:43 +0200

    r03ex08

--- r03ex/ex08/Greeter.java
+++ r03ex/ex08/Greeter.java
@@ -5,10 +5,26 @@ package r03ex.ex08;
  */
 public class Greeter implements Runnable {
 
-    public Greeter(){}
+    private int n = 0;
+    private String target = "";
+
+    public Greeter(int n, String target){
+        this.n = n;
+        this.target = target;
+    }
+
+    public static void main(String[] args) {
+        Greeter g1 = new Greeter(10, "John");
+        Greeter g2 = new Greeter(10, "Ann");
+        g1.run();
+        g2.run();
+    }
+
 
     @Override
     public void run() {
-
+       for(int i = 0; i < n; i++){
+           System.out.println("Witaj " + target);
+       }
     }
 }
